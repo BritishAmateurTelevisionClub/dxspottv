fe39a9e572cc342f9286584dbad77d06d3bfb4d5
Phil Crump <phildcrump@gmail.com>
2013-05-11 00:33:07 +0100

    Test new register page dynamic.

--- js/register.js
+++ js/register.js
@@ -1,7 +1,23 @@
-// Set up buttons
+// Set up button
 //
 $(document).ready(function() {
+	$('#register_form').validate();
 	$('#register_button').button().click( function() {
-    	$('#register_form').submit();
+		$.ajax({
+			url: "/ajax/submit_register.php",
+			data: {
+				fname: $("#fname").val(),
+				callsign: $("#callsign").val(),
+				passwd: $("#passwd").val(),
+				email: $("#passwd").val(),
+				locator: $("#locator").val(),
+				lat: $("#lon").val(),
+				lon: $("#lon").val()
+			},
+			success: function( data ) {
+				console.log(data);
+				var returnJSON = eval('(' + data + ')');
+			}
+		});
 	});
 });
--- register.php
+++ register.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <html>
 <head>
 <title>DXSpot.TV - Register</title>
@@ -5,29 +8,30 @@
 <link href="css/flick/jquery-ui-1.10.3.custom.css" rel="stylesheet">
 <script src="/js/jquery-1.9.1.min.js"></script>
 <script src="/js/jquery-ui-1.10.3.custom.min.js"></script>
+<script src="/js/jquery.validate.js"></script>
 <script type="text/javascript" src="/js/register.js"></script>
 <script type="text/javascript" src="/js/locator.js"></script>
 </head>
 <body>
-<?php
-session_start();
-
-if(!(isset($_REQUEST["callsign"]) && isset($_REQUEST["passwd"]))) {
-?>
 <h2>New User Registration</h2>
 <form id='register_form' action='/register.php' method="post">
-<label class="register_labels"><b>Callsign:</b>&nbsp;</label><input type=text name='callsign' /> Will be converted to Upper case. eg. M0DNY
+	<label class="register_labels"><b>First Name/Nickname:</b>&nbsp;</label>
+	<input type=text name='fname' class="required" minlength="2" />
 <br>
-<label class="register_labels"><b>Password:</b>&nbsp;</label><input type=password name='passwd' />
+	<label class="register_labels"><b>Callsign:</b>&nbsp;</label>
+	<input type=text name='callsign' class="required" minlength="4" /> Will be converted to Upper case. eg. M0DNY
+<br>
+	<label class="register_labels"><b>Password:</b>&nbsp;</label>
+	<input type=password name='passwd' class="required" minlength="5" />
 <br><br>
-<label class="register_labels"><b>Locator:</b>&nbsp;</label><input type=text name='locator' id='locator' onChange=calc_lat_lon() /> Maidenhead eg. IO91HW (Use either 4 or 6 characters)
+<label class="register_labels"><b>Locator:</b>&nbsp;</label><input type=text name='locator' id='locator' onChange=calc_lat_lon() class="required" minlength="4" /> Maidenhead eg. IO91HW (Use either 4 or 6 characters)
 <br>
 <label class="register_labels"><b>Latitude:</b>&nbsp;</label><input type=text name='lat' id='lat' />
 <br>
 <label class="register_labels"><b>Longitude:</b>&nbsp;</label><input type=text name='lon' id='lon' /> (Will fill in automatically from Locator)
 <br>
-<label class="register_labels"><b>Email Address:</b>&nbsp;</label><input type=text name='email' /> Just for administrator contact in case of issues. Not publicly disclosed.
-<br>
+<label class="register_labels"><b>Email Address:</b>&nbsp;</label><input type=text name='email' class="required email" /> Just for administrator contact in case of issues. Not publicly disclosed.
+<br><br>
 <?php
 require_once('recaptchalib.php');
 $publickey = "6LfVM-ESAAAAAIFKeTo0dbqWVOu7c4nd-epDy4qk";
@@ -36,45 +40,8 @@ echo recaptcha_get_html($publickey);
 <br>
 <button class="reduce-font-size" id="register_button">Register</button>
 </form>
-<?php
-} else {
-
-require_once('recaptchalib.php');
-$privatekey = "6LfVM-ESAAAAAJa-5SRWpWMBEOI1z1UNSkVbvqzp";
-$resp = recaptcha_check_answer ($privatekey,
-                                $_SERVER["REMOTE_ADDR"],
-                                $_POST["recaptcha_challenge_field"],
-                                $_POST["recaptcha_response_field"]);
-
-if (!$resp->is_valid) {
-	die ("The reCAPTCHA wasn't entered correctly. Go back and try it again." .
-         "(reCAPTCHA said: " . $resp->error . ")");
-}
-
-include_once("login_functions.php");
-include_once("spot_login.php");
-
-$callsign = escape($dbc, strtoupper($_REQUEST["callsign"]));
-$passwd = escape($dbc, $_REQUEST["passwd"]);
-$locator = escape($dbc, $_REQUEST["locator"]);
-$email = escape($dbc, $_REQUEST["email"]);
-$lat = escape($dbc, $_REQUEST["lat"]);
-$lon = escape($dbc, $_REQUEST["lon"]);
-
-$salt = sha256_salt();
-
-$crypt = crypt($passwd, $salt);
-
-$insert_query="INSERT into users (callsign, password, salt, locator, email, lat, lon) VALUES ('{$callsign}', '{$crypt}', '{$salt}', '{$locator}', '{$email}', '{$lat}', '{$lon}');";
-$ret = mysqli_query($dbc, $insert_query) or die(mysqli_error($dbc));
-?>
 <h2>Registration Successful!</h2>
 <br>
 Click <a href="/">here</a> to return to the map and log in.
-
-<?php
-mysql_end($dbc);
-}
-?>
 </body>
 </html>
