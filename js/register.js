00e7c63df6be30ed216753f0919e34223f671160
Phil Crump <phildcrump@gmail.com>
2013-05-11 00:47:59 +0100

    Bugfix register.

--- js/register.js
+++ js/register.js
@@ -4,15 +4,17 @@ $(document).ready(function() {
 	$('#register_form').validate();
 	$('#register_button').button().click( function() {
 		$.ajax({
-			url: "/ajax/submit_register.php",
+			url: '/ajax/submit_register.php',
 			data: {
-				fname: $("#fname").val(),
-				callsign: $("#callsign").val(),
-				passwd: $("#passwd").val(),
-				email: $("#passwd").val(),
-				locator: $("#locator").val(),
-				lat: $("#lon").val(),
-				lon: $("#lon").val()
+				fname: $('#fname').val(),
+				callsign: $('#callsign').val(),
+				passwd: $('#passwd').val(),
+				email: $('#passwd').val(),
+				locator: $('#locator').val(),
+				lat: $('#lon').val(),
+				lon: $('#lon').val(),
+				recaptcha_challenge_field: $('[name="recaptcha_challenge_field"]').val(),
+				recaptcha_response_field: $('[name="recaptcha_response_field"]').val()
 			},
 			success: function( data ) {
 				console.log(data);
