b4204a983135fff19e4beab430e482e31f712239
Phil Crump <phildcrump@gmail.com>
2013-05-10 15:10:03 +0100

    Freenode => http (non-ssl).

--- index.php
+++ index.php
@@ -64,9 +64,9 @@ if (isset($_COOKIE["auth_error"])) {
 <script type="text/javascript" src="js/map.js"></script>
 <script type="text/javascript">
 <?php if($user_known) { ?> // Do we fill in callsign as nick for irc
-var irc_frame_source = "https://webchat.freenode.net/?channels=#atvspot&nick=<?php print $callsign; ?>&prompt=1"
+var irc_frame_source = "http://webchat.freenode.net/?channels=#atvspot&nick=<?php print $callsign; ?>&prompt=1"
 <?php } else { ?>
-var irc_frame_source = "https://webchat.freenode.net/?channels=#atvspot"
+var irc_frame_source = "http://webchat.freenode.net/?channels=#atvspot"
 <?php } ?> // End of callsign as nick for irc
 </script>
 <script type="text/javascript" src="js/atvspot.js"></script>
