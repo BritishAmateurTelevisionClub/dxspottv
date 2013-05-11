3861653f2536b1bf1a1f16c123ea8bbef8d06a50
Phil Crump <phildcrump@gmail.com>
2013-05-11 23:49:48 +0100

    Remove extra bracket.

--- ajax/update_listening.php
+++ ajax/update_listening.php
@@ -24,7 +24,7 @@ if(mysqli_num_rows ($sessions_result)==0) { // session doesn't exist on server
 		if(mysqli_num_rows ($check_existing_result)==0) {
 			$listen_query = "INSERT into listening (user_id, 70cm_listen, 70cm_freq, 23cm_listen, 23cm_freq, 13cm_listen, 13cm_freq) VALUES ('{$user_id}', '{$l70cm}', '{$l70cm_freq}', '{$l23cm}', '{$l23cm_freq}', '{$l13cm}', '{$l13cm_freq}');";
 		} else {
-			$listen_query = "UPDATE listening SET 70cm_listen='{$l70cm}', 70cm_freq='{$l70cm_freq}', 23cm_listen='{$l23cm}', 23cm_freq='{$l23cm_freq}', 13cm_listen='{$l13cm}', 13cm_freq='{$l13cm_freq}') WHERE user_id='{$user_id}';";
+			$listen_query = "UPDATE listening SET 70cm_listen='{$l70cm}', 70cm_freq='{$l70cm_freq}', 23cm_listen='{$l23cm}', 23cm_freq='{$l23cm_freq}', 13cm_listen='{$l13cm}', 13cm_freq='{$l13cm_freq}' WHERE user_id='{$user_id}';";
         }
         mysqli_query($dbc, $listen_query) or die(mysqli_error($dbc));
 	} else {
