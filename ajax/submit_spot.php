cbc51ca2f87041b1ba0ef06369f5620fcd275aab
Phil Crump <phildcrump@gmail.com>
2013-05-12 11:48:29 +0100

    Fix session behaviour.

--- ajax/submit_spot.php
+++ ajax/submit_spot.php
@@ -25,15 +25,17 @@ if(mysqli_num_rows ($sessions_result)==0) { // session doesn't exist on server
 		$check_existing_user = mysqli_query($dbc, "SELECT id FROM users WHERE callsign='{$r_callsign}';") or die(mysqli_error($dbc));
 		if(mysqli_num_rows ($check_existing_user)==0) { // End User doesn't exist, so add them
 			print 'Adding new unknown callsign.';
-			$new_callsign_query = "INSERT into users (callsign, locator, lat, lon, known) VALUES ('{$r_callsign}', '{$r_locator}', '{$r_lat}', '{$r_lon}', '0');";
-			print $new_callsign_query;
-			mysqli_query($dbc, $new_callsign_query) or die(mysqli_error($dbc));
+			// Insert into users tables
+			mysqli_query($dbc, "INSERT into users (callsign, locator, lat, lon, known) VALUES ('{$r_callsign}', '{$r_locator}', '{$r_lat}', '{$r_lon}', '0');");
+			// Grab allocated user_id
 			$check_existing_user = mysqli_query($dbc, "SELECT id FROM users WHERE callsign='{$r_callsign}';") or die(mysqli_error($dbc));
 		}
 		$check_existing_user_row = mysqli_fetch_array($check_existing_user);
 		$r_userid = $check_existing_user_row['id'];
 		$add_spot_query = "INSERT into spots (mode_id, frequency, primary_id, secondary_id, comments) VALUES ('{$mode_id}', '{$freq}', '{$user_id}', '{$r_userid}', '{$comments}');";
 		mysqli_query($dbc, $add_spot_query) or die(mysqli_error($dbc));
+		// Add activity for remote callsign
+		updateRemoteUserActivity($user_id);
 		print 'Spot Added!';
 	} else {
         print 'Session doesnt match.';
@@ -41,5 +43,13 @@ if(mysqli_num_rows ($sessions_result)==0) { // session doesn't exist on server
 }
 } else { // Not got cookies or variables
 print 'Access Denied.';
+}
+function updateRemoteUserActivity($user_id) {
+	$existing_session_result = mysqli_query($dbc, "SELECT user_id from sessions WHERE user_id='{$user_id}';") or die(mysqli_error($dbc));
+	if(mysqli_num_rows ($existing_session_result)==0) { // No session exists
+		mysqli_query($dbc, "INSERT into sessions (user_id) VALUES ('{$user_id}');") or die(mysqli_error($dbc));
+	} else { // Session exists, update timestamp
+		mysqli_query($dbc, "UPDATE sessions SET activity=NOW() WHERE user_id='{$user_id}';") or die(mysqli_error($dbc));
+	}
 }
 ?>
--- ajax/users.php
+++ ajax/users.php
@@ -4,15 +4,17 @@ include('../spot_login.php');
 
 $output = array();
 $i=1;
-$session_result = mysqli_query($dbc, "SELECT user_id,activity FROM sessions;") or die(mysqli_error($dbc));
-while($session_row = mysqli_fetch_array($session_result))
+$user_result = mysqli_query($dbc, "SELECT callsign,name,lat,lon FROM users WHERE id='{$user_id}';") or die(mysqli_error($dbc));
+while($user_row = mysqli_fetch_array($user_result))
 {
+	// Get session activity data
+	$user_id = $user_row['id'];
+	$session_result = mysqli_query($dbc, "SELECT activity FROM sessions WHERE user_id='{$user_id}';") or die(mysqli_error($dbc));
+	$session_row = mysqli_fetch_array($session_result);
+	$minutes_diff = date_interval_format(date_diff(date_create(), date_create($session_row['activity'])), '%m'); // Used for icons
 	$months_diff = date_interval_format(date_diff(date_create(), date_create($session_row['activity'])), '%m');
 	$hours_diff = date_interval_format(date_diff(date_create(), date_create($session_row['activity'])), '%H');
 	$days_diff = date_interval_format(date_diff(date_create(), date_create($session_row['activity'])), '%d');
-	$user_id = $session_row['user_id'];
-	$user_result = mysqli_query($dbc, "SELECT callsign,lat,lon FROM users WHERE id='{$user_id}';") or die(mysqli_error($dbc));
-	$user_row = mysqli_fetch_array($user_result);
 	$output[$i] = array();
 	$output[$i]['callsign'] = $user_row['callsign'];
 	$output[$i]['latitude'] = $user_row['lat'];
@@ -32,6 +34,7 @@ while($session_row = mysqli_fetch_array($session_result))
 	if($output[$i]['is13cm']==1) {
 		$output[$i]['13cmFreq'] = $listen_row['13cm_freq'];
 	}
+	$output[$i]['minutes_active'] = $minutes_diff;
 	$output[$i]['hours_active'] = $hours_diff;
 	$output[$i]['days_active'] = $days_diff;
 	$output[$i]['months_active'] = $months_diff;
