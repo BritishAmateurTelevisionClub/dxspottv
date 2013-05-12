64130f82a68c48db1735230e10a348d53ddbe183
Phil Crump <phildcrump@gmail.com>
2013-05-12 17:29:42 +0100

    Hide spots if either station is hidden.

--- js/map.js
+++ js/map.js
@@ -29,32 +29,44 @@
       }
 
 function usersShow() {
-	for (var i=0; i<user_markers.length; i++) {
-		user_markers[i].setVisible(true);
-	}
+	changeUsersBandSelect($('#band_select').val());
+	//for (var i=0; i<user_markers.length; i++) {
+	//	user_markers[i].setVisible(true);
+	//}
 	infowindow.close();
 }
 function usersHide() {
 	for (var i=0; i<user_markers.length; i++) {
 		user_markers[i].setVisible(false);
+		for (var j=0; j<spot_lines.length; j++) {
+			if ((spot_lines[j].primary_id == user_markers[i].user_id) || (spot_lines[j].secondary_id == user_markers[i].user_id)) {
+				spot_lines[j].setVisible(false);
+			}
+		}
 	}
 	infowindow.close();
 }
 
 function repeatersShow() {
-	for (var i=0; i<repeater_markers.length; i++) {
-		repeater_markers[i].setVisible(true);
-	}
+	changeRepeatersBandSelect($('#band_select').val());
+	//for (var i=0; i<repeater_markers.length; i++) {
+	//	repeater_markers[i].setVisible(true);
+	//}
 	infowindow.close();
 }
 function repeatersHide() {
 	for (var i=0; i<repeater_markers.length; i++) {
 		repeater_markers[i].setVisible(false);
+		for (var j=0; j<spot_lines.length; j++) {
+			if (spot_lines[j].secondary_id == repeaters_markers[i].repeater_id) {
+				spot_lines[j].setVisible(false);
+			}
+		}
 	}
 	infowindow.close();
 }
 
-      function myclick(i) {
-        google.maps.event.trigger(gmarkers[i],"click");
-      }
+function myclick(i) {
+	google.maps.event.trigger(gmarkers[i],"click");
+}
 
