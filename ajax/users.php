86af77b386d6038cba00cbbed2cdc0a9abf53160
Phil Crump <phildcrump@gmail.com>
2013-05-16 17:04:06 +0100

    We know activity of unknown users.

--- ajax/users.php
+++ ajax/users.php
@@ -14,7 +14,15 @@ while($user_row = mysqli_fetch_array($user_result))
 	$output[$i]['latitude'] = $user_row['lat'];
 	$output[$i]['longitude'] = $user_row['lon'];
 	$output[$i]['longitude'] = $user_row['lon'];
+	// Get User activity Data
+	$session_result = mysqli_query($dbc, "SELECT activity FROM sessions WHERE user_id='{$user_id}';") or die(mysqli_error($dbc));
+	$session_row = mysqli_fetch_array($session_result);
+	$output[$i]['seconds_active'] = time() - date_format(date_create($session_row['activity']),'U'); // Used for icons
+	$output[$i]['months_active'] = date_interval_format(date_diff(date_create(), date_create($session_row['activity'])), '%m');
+	$output[$i]['hours_active'] = date_interval_format(date_diff(date_create(), date_create($session_row['activity'])), '%h');
+	$output[$i]['days_active'] = date_interval_format(date_diff(date_create(), date_create($session_row['activity'])), '%d');
 	if($user_row['known']=='1') {
+		$output[$i]['known'] = '1';
 		// Get User Listening Data
 		$listen_result = mysqli_query($dbc, "SELECT * FROM listening WHERE user_id='{$user_id}';") or die(mysqli_error($dbc));
 		$listen_row = mysqli_fetch_array($listen_result);
@@ -30,14 +38,6 @@ while($user_row = mysqli_fetch_array($user_result))
 		if($output[$i]['is13cm']==1) {
 			$output[$i]['13cmFreq'] = $listen_row['13cm_freq'];
 		}
-		// Get User activity Data
-		$session_result = mysqli_query($dbc, "SELECT activity FROM sessions WHERE user_id='{$user_id}';") or die(mysqli_error($dbc));
-		$session_row = mysqli_fetch_array($session_result);
-		$output[$i]['known'] = '1';
-		$output[$i]['seconds_active'] = time() - date_format(date_create($session_row['activity']),'U'); // Used for icons
-		$output[$i]['months_active'] = date_interval_format(date_diff(date_create(), date_create($session_row['activity'])), '%m');
-		$output[$i]['hours_active'] = date_interval_format(date_diff(date_create(), date_create($session_row['activity'])), '%h');
-		$output[$i]['days_active'] = date_interval_format(date_diff(date_create(), date_create($session_row['activity'])), '%d');
 	} else {
 		$output[$i]['known'] = '0';
 	}
