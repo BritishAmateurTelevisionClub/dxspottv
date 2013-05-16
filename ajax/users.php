9be95740d71ffa4eaf974c45a6342e412f5f0076
Phil Crump <phildcrump@gmail.com>
2013-05-16 16:28:47 +0100

    Patch users.php for known-ness.

--- ajax/users.php
+++ ajax/users.php
@@ -7,39 +7,40 @@ $i=1;
 $user_result = mysqli_query($dbc, "SELECT id,callsign,name,lat,lon,known FROM users;") or die(mysqli_error($dbc));
 while($user_row = mysqli_fetch_array($user_result))
 {
-	// Get session activity data
 	$user_id = $user_row['id'];
-	$session_result = mysqli_query($dbc, "SELECT activity FROM sessions WHERE user_id='{$user_id}';") or die(mysqli_error($dbc));
-	$session_row = mysqli_fetch_array($session_result);
-	$seconds_diff = time() - date_format(date_create($session_row['activity']),'U'); // Used for icons
-	$months_diff = date_interval_format(date_diff(date_create(), date_create($session_row['activity'])), '%m');
-	$hours_diff = date_interval_format(date_diff(date_create(), date_create($session_row['activity'])), '%h');
-	$days_diff = date_interval_format(date_diff(date_create(), date_create($session_row['activity'])), '%d');
 	$output[$i] = array();
 	$output[$i]['id'] = $user_id;
 	$output[$i]['callsign'] = $user_row['callsign'];
 	$output[$i]['latitude'] = $user_row['lat'];
 	$output[$i]['longitude'] = $user_row['lon'];
 	$output[$i]['longitude'] = $user_row['lon'];
-	$output[$i]['known'] = $user_row['known'];
-	$listen_result = mysqli_query($dbc, "SELECT * FROM listening WHERE user_id='{$user_id}';") or die(mysqli_error($dbc));
-	$listen_row = mysqli_fetch_array($listen_result);
-	$output[$i]['is70cm'] = $listen_row['70cm_listen'];
-	if($output[$i]['is70cm']==1) {
-		$output[$i]['70cmFreq'] = $listen_row['70cm_freq'];
+	if($user_row['known']=='1') {
+		// Get User Listening Data
+		$listen_result = mysqli_query($dbc, "SELECT * FROM listening WHERE user_id='{$user_id}';") or die(mysqli_error($dbc));
+		$listen_row = mysqli_fetch_array($listen_result);
+		$output[$i]['is70cm'] = $listen_row['70cm_listen'];
+		if($output[$i]['is70cm']==1) {
+			$output[$i]['70cmFreq'] = $listen_row['70cm_freq'];
+		}
+		$output[$i]['is23cm'] = $listen_row['23cm_listen'];
+		if($output[$i]['is23cm']==1) {
+			$output[$i]['23cmFreq'] = $listen_row['23cm_freq'];
+		}
+		$output[$i]['is13cm'] = $listen_row['13cm_listen'];
+		if($output[$i]['is13cm']==1) {
+			$output[$i]['13cmFreq'] = $listen_row['13cm_freq'];
+		}
+		// Get User activity Data
+		$session_result = mysqli_query($dbc, "SELECT activity FROM sessions WHERE user_id='{$user_id}';") or die(mysqli_error($dbc));
+		$session_row = mysqli_fetch_array($session_result);
+		$output[$i]['known'] = '1';
+		$output[$i]['seconds_active'] = time() - date_format(date_create($session_row['activity']),'U'); // Used for icons
+		$output[$i]['months_active'] = date_interval_format(date_diff(date_create(), date_create($session_row['activity'])), '%m');
+		$output[$i]['hours_active'] = date_interval_format(date_diff(date_create(), date_create($session_row['activity'])), '%h');
+		$output[$i]['days_active'] = date_interval_format(date_diff(date_create(), date_create($session_row['activity'])), '%d');
+	} else {
+		$output[$i]['known'] = '0';
 	}
-	$output[$i]['is23cm'] = $listen_row['23cm_listen'];
-	if($output[$i]['is23cm']==1) {
-		$output[$i]['23cmFreq'] = $listen_row['23cm_freq'];
-	}
-	$output[$i]['is13cm'] = $listen_row['13cm_listen'];
-	if($output[$i]['is13cm']==1) {
-		$output[$i]['13cmFreq'] = $listen_row['13cm_freq'];
-	}
-	$output[$i]['seconds_active'] = $seconds_diff;
-	$output[$i]['hours_active'] = $hours_diff;
-	$output[$i]['days_active'] = $days_diff;
-	$output[$i]['months_active'] = $months_diff;
 	$i++;
 }
 
