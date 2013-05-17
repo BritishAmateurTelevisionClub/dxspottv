bc2ab118c6a3a5e35721dea35f807b3fbbb52311
Phil Crump <phildcrump@gmail.com>
2013-05-17 01:28:08 +0100

    Fix: Fix global spot log date.

--- js/atvspot-ui.js
+++ js/atvspot-ui.js
@@ -144,7 +144,7 @@ function createGlobalSpotLog(spotLog) {
 				return e.user_id == spot.secondary_id;
 			});
 		}
-		spotLogDivContent+=parseInt(spot_data['time'].substr(8,2))+"&nbsp;"+months[parseInt(spot_data['time'].substr(5,2))]+"&nbsp;"+spot_data['time'].substr(11,8)+":&nbsp;<b>"+primary_search[0].callsign+"</b>-><b>"+secondary_search[0].callsign+"</b>";
+		spotLogDivContent+=parseInt(spot['time'].substr(8,2))+"&nbsp;"+months[parseInt(spot['time'].substr(5,2))]+"&nbsp;"+spot['time'].substr(11,8)+":&nbsp;<b>"+primary_search[0].callsign+"</b>-><b>"+secondary_search[0].callsign+"</b>";
 		spotLogDivContent+="&nbsp;Frequency:&nbsp;"+spot['frequency']+"MHz";
 		if(spot['comments'].length != 0) {
 			spotLogDivContent+="<br>";
