ba2558cff2ae3b41d947a8172975c598e3475856
Phil Crump <phildcrump@gmail.com>
2013-05-18 19:16:33 +0100

    Retry midpoint position function.

--- js/atvspot.js
+++ js/atvspot.js
@@ -177,9 +177,9 @@ function createSpotLine(spot_data) {
 	var infoContent = spotLine.date+"<br><b>"+primary_callsign+"</b>&nbsp;->&nbsp;"+"<b>"+secondary_callsign+"</b><br>"+spotLine.frequency+"&nbsp;MHz";
 	
 	
-	google.maps.event.addListener(spotLine, 'mouseover', function(e) {
+	google.maps.event.addListener(spotLine, 'mouseover', function() {
 		infowindow.setContent(infoContent);
-		infowindow.setPosition(e.latLng);
+		infowindow.setPosition(new google.maps.LatLng((primary_latlon.lat() + primary_latlon.lat())/2, (secondary_latlon.lng() + secondary_latlon.lng())/2));
     	infowindow.open(map);
    	});
    	
