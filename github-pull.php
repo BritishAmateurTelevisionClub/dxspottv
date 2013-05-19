5c16eefe273e3085097cd550f66df26459a860fa
Phil Crump <pdc1g09@soton.ac.uk>
2013-05-19 23:28:14 +0200

    Add echo of output to github-pull script.

--- github-pull.php
+++ github-pull.php
@@ -1,7 +1,8 @@
 <?php
 if (isset($_REQUEST['pass'])) {
     if ($_REQUEST['pass']=="updateMeN0w") {
-        shell_exec('cd /srv/www.dxspot.tv/ && git pull');
+        $output = shell_exec('cd /srv/www.dxspot.tv/ && git pull');
+        echo "<pre>$output</pre>";
     }
 }
 ?>
