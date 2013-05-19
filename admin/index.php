48bd7187f547d30fefd36d8f513f2e59b188baa2
Phil Crump <phildcrump@gmail.com>
2013-05-19 14:57:58 +0100

    Add admin index page.

---
+++ admin/index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>DXSpot.TV Admin</title>
+</head>
+<body>
+<h2>DXSpot.TV Admin Pages</h2>
+<br>
+<a href="repeaters.php"><h4>Repeater List Administration</h4></a>
+<br>
+<a href="http://webirc.dxspot.tv/adminengine"><h4>qwebirc Admin Page</h4></a>
+
+</body>
+</html>
