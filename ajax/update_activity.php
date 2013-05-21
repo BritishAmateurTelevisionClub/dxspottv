9963594aafeb98be1d76b84c634dd50ac04ffca2
Phil Crump <pdc1g09@soton.ac.uk>
2013-05-21 21:11:19 +0100

    Fix activity refresh clobbering session.

--- ajax/update_activity.php
+++ ajax/update_activity.php
@@ -12,15 +12,8 @@ if($got_cookies) {
 		$target_row = mysqli_fetch_array($sessions_result);
 		if ($_COOKIE["session_key"]==$target_row["session_id"]) {
 			// Session matches, so is logged in!
-			$user_id = $_COOKIE["user_id"];
-			$sessions_result = mysqli_query($dbc, "SELECT session_id FROM sessions WHERE user_id='" . $user_id . "';") or die(mysqli_error($dbc));
-			if(mysqli_num_rows ($sessions_result)==0) {
-				$insert_query="INSERT into sessions (session_id, user_id) VALUES ('{$session_key}', '{$user_id}');";
-				$ret = mysqli_query($dbc, $insert_query) or die(mysqli_error($dbc));
-			} else {
-				$update_query="UPDATE sessions set session_id='{$session_key}',activity=NOW() where user_id = '{$user_id}';";
-				$ret = mysqli_query($dbc, $update_query) or die(mysqli_error($dbc));
-			}
+			$update_query="UPDATE sessions set activity=NOW() where user_id = '{$_COOKIE["user_id"]}';";
+		    mysqli_query($dbc, $update_query) or die(mysqli_error($dbc));
 		} else {
 			print 'Session doesnt match.';
 		}
