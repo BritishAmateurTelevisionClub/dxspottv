cc1341f466a6c7318df060fcd902797c4084b537
Phil Crump <phildcrump@gmail.com>
2013-05-25 14:15:38 +0100

    Fix class name.

--- scrape_repeaters.php
+++ scrape_repeaters.php
@@ -4,7 +4,7 @@ include_once 'simple_html_dom.php';
 $html = file_get_html("http://www.ukrepeater.net/repeaterlist5.htm");
 
 foreach($html->find('table.mixed tr') as $row) {
-	foreach($row->find('td.class=minil') as $cell) {
+	foreach($row->find('td.minil') as $cell) {
 		print "<pre>" + $cell->plaintext + "</pre><br>";
 	}
 }
