9f25e5a227c25dbc6a7ddc7fc5eb4f4624fe6440
Phil Crump <phildcrump@gmail.com>
2013-05-25 14:20:25 +0100

    Use proper concatenation.

--- scrape_repeaters.php
+++ scrape_repeaters.php
@@ -2,12 +2,12 @@
 include_once 'simple_html_dom.php';
 
 $html = file_get_html("http://www.ukrepeater.net/repeaterlist5.htm");
-print "HTML: " + $html;
+print "HTML: " . $html;
 foreach($html->find('table.mixed tr') as $row) {
-	print "Row: " + $row;
+	print "Row: " . $row;
 	foreach($row->find('td.minil') as $cell) {
 		print $cell;
-		print "<pre>" + $cell->plaintext + "</pre><br>";
+		print "<pre>" . $cell->plaintext . "</pre><br>";
 	}
 }
 ?>
