e68570c2b4358bc39a3e8c80a58d47eafb91721e
Phil Crump <phildcrump@gmail.com>
2013-05-25 14:34:53 +0100

    Remove more debugging.

--- scrape_repeaters.php
+++ scrape_repeaters.php
@@ -4,9 +4,7 @@ include_once 'simple_html_dom.php';
 $html = file_get_html("http://www.ukrepeater.net/repeaterlist5.htm");
 
 foreach($html->find('table.mix tr') as $row) {
-	print "Row: " . $row;
 	foreach($row->find('td.minil') as $cell) {
-		print $cell;
 		print "<pre>" . $cell->plaintext . "</pre><br>";
 	}
 }
