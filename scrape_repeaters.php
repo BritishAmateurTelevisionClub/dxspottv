d5fa1961f964be21c390c81787cf4d117c57f388
Phil Crump <phildcrump@gmail.com>
2013-05-25 14:14:31 +0100

    Fix objects.

--- scrape_repeaters.php
+++ scrape_repeaters.php
@@ -3,9 +3,7 @@ include_once 'simple_html_dom.php';
 
 $html = file_get_html("http://www.ukrepeater.net/repeaterlist5.htm");
 
-$table = $html->find('table.mixed', 0);
-
-foreach($table->find('tr') as $row) {
+foreach($html->find('table.mixed tr') as $row) {
 	foreach($row->find('td.class=minil') as $cell) {
 		print "<pre>" + $cell->plaintext + "</pre><br>";
 	}
