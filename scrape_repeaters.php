bd99aad46e68b9dc3bdfd24149f8cb04c13ea3fe
Phil Crump <phildcrump@gmail.com>
2013-05-25 15:00:07 +0100

    Add more fields to repeater scrape.

--- scrape_repeaters.php
+++ scrape_repeaters.php
@@ -8,8 +8,23 @@ foreach($html->find('table.mix tr') as $row) {
 	$output[$i] = array();
 	$output[$i]['callsign'] = $row->find('td.minil', 0)->plaintext;
 	$output[$i]['band'] = $row->find('td.minil', 1)->plaintext;
-	$output[$i]['output_freq'] = $row->find('td.minil', 2)->plaintext;
-	$output[$i]['input_freq1'] = $row->find('td.minil', 3)->plaintext;
+	$output[$i]['tx_freq'] = $row->find('td.minil', 2)->plaintext;
+	$output[$i]['rx_freq1'] = $row->find('td.minil', 3)->plaintext;
+	$output[$i]['rx_freq2'] = $row->find('td.minil', 4)->plaintext;
+	$output[$i]['qthr'] = $row->find('td.minil', 5)->plaintext;
+	$output[$i]['qth'] = $row->find('td.minil', 6)->plaintext;
+	$output[$i]['ngr'] = $row->find('td.minil', 7)->plaintext;
+	$output[$i]['region'] = $row->find('td.minil', 8)->plaintext;
+	$output[$i]['keeper'] = $row->find('td.minil', 9)->plaintext;
+	$latlon = explode(",",$row->find('td.minil', 10)->plaintext);
+	$output[$i]['lat'] = $latlon[0];
+	$output[$i]['lon'] = $latlon[1];
+	$status_text = $row->find('td.minil', 11)->plaintext;
+	if($status_text=="OPERATIONAL") {
+		$output[$i]['status'] = 1; // Online
+	} else {
+		$output[$i]['status'] = 0; // Offline
+	}
 	$i++;
 }
 print json_encode($output);
