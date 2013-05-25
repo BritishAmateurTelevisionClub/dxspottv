e76ec7dc27649e723c7a450b1d5ed7454c2e947e
Phil Crump <phildcrump@gmail.com>
2013-05-25 14:43:20 +0100

    Add JSON output to repeater scrape.

--- scrape_repeaters.php
+++ scrape_repeaters.php
@@ -2,10 +2,15 @@
 include_once 'simple_html_dom.php';
 
 $html = file_get_html("http://www.ukrepeater.net/repeaterlist5.htm");
-
+$output = array();
+$i=1;
 foreach($html->find('table.mix tr') as $row) {
-	foreach($row->find('td.minil') as $cell) {
-		print "<pre>" . $cell->plaintext . "</pre><br>";
-	}
+	$output[$i] = array();
+	$output[$i]['callsign'] = $row->find('td.minil', 0)->plaintext;
+	$output[$i]['band'] = $row->find('td.minil', 1)->plaintext;
+	$output[$i]['output_freq'] = $row->find('td.minil', 2)->plaintext;
+	$output[$i]['input_freq1'] = $row->find('td.minil', 3)->plaintext;
+	$i++;
 }
+print json_encode($output);
 ?>
