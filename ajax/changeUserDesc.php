eac0712b6a22b2175c5b793b1bb748af9ac9d3af
Phil Crump <phildcrump@gmail.com>
2013-05-28 00:55:35 +0100

    Overhaul changeUserDesc.php #7

--- ajax/changeUserDesc.php
+++ ajax/changeUserDesc.php
@@ -3,21 +3,27 @@ session_start();
 $got_cookies = (isset($_COOKIE["user_id"]) && isset($_COOKIE["session_key"]));
 $got_variable = isset($_REQUEST["description"]);
 if($got_cookies && $got_variable) {
-	include('spot_login.php');
-	$desc = htmlentities(mysqli_real_escape_string($dbc, $_REQUEST["description"]));
-	$sessions_result = mysqli_query($dbc, "SELECT session_id FROM sessions WHERE user_id='" . $_COOKIE["user_id"] . "';") or die(mysqli_error($dbc));  
-	if(mysqli_num_rows ($sessions_result)==0) { // session doesn't exist on server
+	$desc = htmlentities($_REQUEST["description"]);
+	require_once('spot_login.php');
+	$sessions_statement = $dbc->prepare("SELECT session_id FROM sessions WHERE user_id=?;");
+	$sessions_statement->bind_param('i', $_COOKIE["user_id"]);
+	$sessions_statement->execute();
+	$sessions_statement->bind_result($sessions_result);
+	$sessions_statement->fetch();
+	if($sessions_statement->num_rows!=1) { // session doesn't exist on server
 		print 'Session not found.';
 	} else {
-		$target_row = mysqli_fetch_array($sessions_result);
-		if ($_COOKIE["session_key"]==$target_row["session_id"]) {
+		if ($_COOKIE["session_key"]==$sessions_result) {
 			// Session matches, so is logged in!
-			$update_query="UPDATE users set station_desc='{$desc}' WHERE id = '{$_COOKIE["user_id"]}';";
-		    mysqli_query($dbc, $update_query) or die(mysqli_error($dbc));
+			$update_statement = $dbc->prepare("UPDATE users set station_desc=? WHERE id=?;");
+			$update_statement->bind_param('si', $desc, $_COOKIE["user_id"]);
+			$update_statement->execute();
+			$update_statement->close();
 		} else {
 			print 'Session doesnt match.';
 		}
 	}
+	$sessions_statement->close();
 	mysql_end($dbc);
 } else { // Not got cookies
 	print 'Access Denied.';
