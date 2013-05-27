444c17978685b86fee4cecfaa9743f562348f9be
Phil Crump <phildcrump@gmail.com>
2013-05-27 22:00:02 +0100

    Use totalUsers.

--- admin/index.php
+++ admin/index.php
@@ -14,6 +14,7 @@ $(document).ready(function() {
     		$('#lastHourCount').html('<b>'+activeUserData['lastHour']+'</b>');
     		$('#lastDayCount').html('<b>'+activeUserData['lastDay']+'</b>');
     		$('#lastWeekCount').html('<b>'+activeUserData['lastWeek']+'</b>');
+    		$('#totalUsers').html('<b>'+activeUserData['totalUsers']+'</b>');
 		}
 	});
 });
@@ -26,12 +27,13 @@ $(document).ready(function() {
 
 <a href="http://webirc.dxspot.tv/adminengine"><h4>qwebirc Admin Page</h4></a>
 
-<h4>Current User Stats:</h4>
+<h4>User Stats:</h4>
 <ul>
 <li>Active in Last Minute: <span id="lastMinCount">loading...</span></li>
 <li>Active in Last Hour: <span id="lastHourCount">loading...</span></li>
 <li>Active in Last Day: <span id="lastDayCount">loading...</span></li>
 <li>Active in Last Week: <span id="lastWeekCount">loading...</span></li>
+<li>Total Users: <span id="totalUsers">loading...</span></li>
 </ul>
 </body>
 </html>
