cf3deb8da9d408f1cc46158a8567db1ebd2e653d
Phil Crump <phildcrump@gmail.com>
2013-05-27 19:41:30 +0100

    Add current stats to admin index.

--- admin/index.php
+++ admin/index.php
@@ -3,6 +3,20 @@
 <head>
 <meta charset="utf-8">
 <title>DXSpot.TV Admin</title>
+<script src="/js/jquery-1.9.1.min.js"></script>
+<script>
+$(document).ready(function() {
+	$.ajax({
+		url: "/ajax/currentActiveUsers.php",
+		success: function( data ) {
+			myJSONObject = eval('(' + data + ')');
+    		$('#lastMinCount').val(myJSONObject['lastMinute']);
+    		$('#lastHourCount').val(myJSONObject['lastHour']);
+    		$('#lastDayCount').val(myJSONObject['lastDay']);
+		}
+	});
+}
+</script>
 </head>
 <body>
 <h2>DXSpot.TV Admin Pages</h2>
@@ -11,5 +25,11 @@
 
 <a href="http://webirc.dxspot.tv/adminengine"><h4>qwebirc Admin Page</h4></a>
 
+<h4>Current Stats:</h4>
+<ul>
+<li>Last Minute: <span id="lastMinCount">loading...</span></li>
+<li>Last Hour: <span id="lastHourCount">loading...</span></li>
+<li>Last Day: <span id="lastDayCount">loading...</span></li>
+</ul>
 </body>
 </html>
