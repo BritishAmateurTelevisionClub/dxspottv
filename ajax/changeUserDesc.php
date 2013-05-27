19c3cea874ba91f6f8889c6af4582f2723434f87
Phil Crump <phildcrump@gmail.com>
2013-05-27 15:45:58 +0100

    Add ajax file to git.

---
+++ ajax/changeUserDesc.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+$got_cookies = (isset($_COOKIE["user_id"]) && isset($_COOKIE["session_key"]));
+$got_variable = isset($_REQUEST["description"]);
+if($got_cookies && $got_variable) {
+	include('spot_login.php');
+	$desc = mysqli_real_escape_string($dbc, $_REQUEST["description"]);
+	$sessions_result = mysqli_query($dbc, "SELECT session_id FROM sessions WHERE user_id='" . $_COOKIE["user_id"] . "';") or die(mysqli_error($dbc));  
+	if(mysqli_num_rows ($sessions_result)==0) { // session doesn't exist on server
+		print 'Session not found.';
+	} else {
+		$target_row = mysqli_fetch_array($sessions_result);
+		if ($_COOKIE["session_key"]==$target_row["session_id"]) {
+			// Session matches, so is logged in!
+			$update_query="UPDATE users set station_desc='{$desc}' where user_id = '{$_COOKIE["user_id"]}';";
+		    mysqli_query($dbc, $update_query) or die(mysqli_error($dbc));
+		} else {
+			print 'Session doesnt match.';
+		}
+	}
+	mysql_end($dbc);
+} else { // Not got cookies
+	print 'Access Denied.';
+}
+?>
