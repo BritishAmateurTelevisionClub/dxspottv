02674a981327f6c48b8dc7d5706e978833071fe1
Phil Crump <phildcrump@gmail.com>
2013-05-27 12:00:09 +0100

    Add more repeater data to mapData.php.

--- ajax/mapData.php
+++ ajax/mapData.php
@@ -44,12 +44,27 @@ if(apc_exists('mapData')) {
 		$repeater['description'] = $row['description'];
 		$repeater['latitude'] = $row['lat'];
 		$repeater['longitude'] = $row['lon'];
+		$repeater['qth_r'] = $row['qth_r'];
+		$repeater['qth'] = $row['qth'];
+		$repeater['tx_freq'] = $row['tx_freq'];
+		$repeater['rx_freq'] = $row['rx_freq'];
+		if($row['rx_freq_2']!=NULL) {
+			$repeater['rx_freq_2'] = $row['rx_freq_2'];
+		}
+		if($row['alt_tx_freq']!=NULL) {
+			$repeater['alt_tx_freq'] = $row['alt_tx_freq'];
+			$repeater['alt_rx_freq'] = $row['alt_rx_freq'];
+			if($row['alt_rx_freq_2']!=NULL) {
+				$repeater['alt_rx_freq_2'] = $row['alt_rx_freq_2'];
+			}
+		}
 		$repeater['is_70cm'] = $row['is_70cm'];
 		$repeater['is_23cm'] = $row['is_23cm'];
 		$repeater['is_13cm'] = $row['is_13cm'];
 		$repeater['is_9cm'] = $row['is_9cm'];
 		$repeater['is_6cm'] = $row['is_6cm'];
 		$repeater['is_3cm'] = $row['is_3cm'];
+		$repeater['description'] = $row['description'];
 		$repeater['active'] = $row['active'];
 		$output[] = $repeater;
 		unset($repeater);
