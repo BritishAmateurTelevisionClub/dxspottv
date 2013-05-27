048bd37e330579a33880226e2d86760460bed4dd
Phil Crump <phildcrump@gmail.com>
2013-05-27 21:55:40 +0100

    Use lastWeek data on admin index.

--- admin/index.php
+++ admin/index.php
@@ -9,10 +9,11 @@ $(document).ready(function() {
 	$.ajax({
 		url: "/ajax/currentActiveUsers.php",
 		success: function( data ) {
-			myJSONObject = eval('(' + data + ')');
-    		$('#lastMinCount').html('<b>'+myJSONObject['lastMinute']+'</b>');
-    		$('#lastHourCount').html('<b>'+myJSONObject['lastHour']+'</b>');
-    		$('#lastDayCount').html('<b>'+myJSONObject['lastDay']+'</b>');
+			activeUserData = eval('(' + data + ')');
+    		$('#lastMinCount').html('<b>'+activeUserData['lastMinute']+'</b>');
+    		$('#lastHourCount').html('<b>'+activeUserData['lastHour']+'</b>');
+    		$('#lastDayCount').html('<b>'+activeUserData['lastDay']+'</b>');
+    		$('#lastWeekCount').html('<b>'+activeUserData['lastWeek']+'</b>');
 		}
 	});
 });
@@ -25,11 +26,12 @@ $(document).ready(function() {
 
 <a href="http://webirc.dxspot.tv/adminengine"><h4>qwebirc Admin Page</h4></a>
 
-<h4>Current Stats:</h4>
+<h4>Current User Stats:</h4>
 <ul>
-<li>Last Minute: <span id="lastMinCount">loading...</span></li>
-<li>Last Hour: <span id="lastHourCount">loading...</span></li>
-<li>Last Day: <span id="lastDayCount">loading...</span></li>
+<li>Active in Last Minute: <span id="lastMinCount">loading...</span></li>
+<li>Active in Last Hour: <span id="lastHourCount">loading...</span></li>
+<li>Active in Last Day: <span id="lastDayCount">loading...</span></li>
+<li>Active in Last Week: <span id="lastWeekCount">loading...</span></li>
 </ul>
 </body>
 </html>
