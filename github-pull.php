1c654e8273c271aa685ec3eca3c211e952db0a37
Phil Crump <phildcrump@gmail.com>
2013-05-28 01:26:23 +0100

    Remove old github pull script for now.

--- github-pull.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (isset($_REQUEST['pass'])) {
-    if ($_REQUEST['pass']=="updateMeN0w") {
-        $output = shell_exec('cd /srv/www.dxspot.tv/ && git pull 2>&1');
-        echo "<pre>$output</pre>";
-    }
-}
-?>
