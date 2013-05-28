4102c9920a1d9885be7e0fc9728c908b43d68fcf
Phil Crump <phildcrump@gmail.com>
2013-05-28 02:35:15 +0100

    Add verbose output to combine.sh

--- js/combine.sh
+++ js/combine.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+echo "Removing old files.."
+rm -fv atvspot-combined*
+echo "Combining js.."
 cat atvspot.js atvspot-ajax.js atvspot-ui.js atvspot-util.js locator.js map.js > atvspot-combined.js
+echo "Minimising.."
 python closure.py http://www.dxspot.tv/js/atvspot-combined.js > atvspot-combined.min.js
+echo "gzipping.."
 cp atvspot-combined.min.js atvspot-combined.min.js.1
 gzip -9 atvspot-combined.min.js.1
 mv atvspot-combined.min.js.1.gz atvspot-combined.min.js.gz
+echo "done."
