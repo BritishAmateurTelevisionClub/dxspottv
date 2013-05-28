4424996c606e4d67d6da70ca13e77f9797d1c7b9
Phil Crump <phildcrump@gmail.com>
2013-05-28 15:01:57 +0100

    Restructure admin.

--- admin/repeaters.php
+++ admin/repeaters.php
@@ -1,15 +1,12 @@
-<?php
-//session_start();
-?>
 <!doctype html>
 <html lang="en">
 <head>
 <meta charset="utf-8">
 <title>Repeater Management</title>
-<script src="/js/jquery-1.9.1.min.js"></script>
 </head>
 <body>
 <h1>Admin</h1>
+<a href="/admin/index.html"><h3>Back to Index</h2></a>
 <h2>Repeaters</h2>
 <ul>
 <?php
@@ -18,7 +15,7 @@ $repeater_result = mysqli_query($dbc, "SELECT id,callsign FROM repeaters;") or d
 while($row = mysqli_fetch_array($repeater_result))
 {
 ?>
-<li><a href="edit_repeater.php?id=<?php print $row['id']; ?>"><?php print $row['callsign']; ?></a>
+<li><a href="editRepeater.html?id=<?php print $row['id']; ?>"><?php print $row['callsign']; ?></a>
 <?php
 }
 mysql_end($dbc);
