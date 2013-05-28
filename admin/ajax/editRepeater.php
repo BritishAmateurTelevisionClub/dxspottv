318d68a6b470b86685b854ea242949c7aa77c214
Phil Crump <phildcrump@gmail.com>
2013-05-28 13:06:00 +0100

    Fix functions in editRepeater.

--- admin/ajax/editRepeater.php
+++ admin/ajax/editRepeater.php
@@ -8,14 +8,14 @@ if($got_variables) {
 
 	$update_statement = $dbc->prepare("UPDATE repeaters set callsign=?,qth_r=?,qth=?,description=?,website=?,keeper=?,active=? WHERE id=?;");
 	$update_statement->bind_param('ssssssi',
-		$htmlentities($_REQUEST["callsign"]),
-		$htmlentities($_REQUEST["locator"]),
-		$htmlentities($_REQUEST["location"]),
-		$htmlentities($_REQUEST["description"]),
-		$htmlentities($_REQUEST["website"]),
-		$htmlentities($_REQUEST["keeper"]),
-		$htmlentities($_REQUEST["active"]),
-		$htmlentities($_REQUEST["repeater"]));
+		htmlentities($_REQUEST["callsign"]),
+		htmlentities($_REQUEST["locator"]),
+		htmlentities($_REQUEST["location"]),
+		htmlentities($_REQUEST["description"]),
+		htmlentities($_REQUEST["website"]),
+		htmlentities($_REQUEST["keeper"]),
+		htmlentities($_REQUEST["active"]),
+		htmlentities($_REQUEST["repeater"]));
 	$update_statement->execute();
 	$update_statement->close();
 	
