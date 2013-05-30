c64e6a1e1fd8e91eed7a8c644925e728f1db4a78
Phil Crump <phildcrump@gmail.com>
2013-05-30 17:14:22 +0100

    Shift automatic append.

--- js/atvspot.js
+++ js/atvspot.js
@@ -70,7 +70,7 @@ function createUserMarker(user_data) {
     marker.activity = user_data['seconds_active'];
     marker.known = user_data['known'];
     marker.station_desc = user_data['desc'];
-    marker.station_website = user_data['website'];
+    marker.station_website = "http://"+user_data['website'];
     user_markers.push(marker);
     
     var infoTab = '<div class="user_bubble_info">'+
@@ -86,7 +86,7 @@ function createUserMarker(user_data) {
     var descTab = '<div class="user_bubble_desc">'+
         marker.station_desc;
     if(marker.station_website!='') {
-    	descTab += '<br><br><a href="http://'+marker.station_website+'" target="_blank"><b>'+marker.station_website+'</b></a>';
+    	descTab += '<br><br><a href="'+marker.station_website+'" target="_blank"><b>'+marker.station_website+'</b></a>';
     }
     descTab += '</div>';
     
