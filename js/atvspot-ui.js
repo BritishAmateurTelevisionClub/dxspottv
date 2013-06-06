32510593b670101b0d21ebd34da8cf6243cf59a7
Phil Crump <phildcrump@gmail.com>
2013-06-06 09:48:45 +0100

    Default checkbox to unticked with jquery.

--- js/atvspot-ui.js
+++ js/atvspot-ui.js
@@ -98,6 +98,7 @@ $(document).ready(function() {
 
 // Radio active status
 $(document).ready(function() {
+	$('#radioBox').prop('checked', false);
 	$('#radioBox').change(function() {
 		$('#changeRadioStatus').show();
     	$('#changeRadioStatus').html("<font color=green>Changing..</font>");
