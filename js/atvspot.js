4cf7ee7477ceb2e0a17af3215b8d16a643067119
Phil Crump <phildcrump@gmail.com>
2013-06-12 21:38:35 +0100

    Add map right-click.

--- js/atvspot.js
+++ js/atvspot.js
@@ -47,6 +47,21 @@ function initialize() {
 	google.maps.event.addListener(map, 'click', function() {
 		infowindow.close();
 	});
+	
+	google.maps.event.addListener(map, "rightclick", function(event) {
+		var randomLoc = CoordToLoc(event.latLng.lat(), event.latLng.lng());
+		infoContent="<h3 style='line-height: 0.3em;'>"+randomLoc+"</h3>";
+		if(logged_in) {
+			var elevation_vars = "'"+user_callsign+"','"+user_lat+"','"+user_lon+"','"+randomLoc+"','"+event.latLng.lat()+"','"+event.latLng.lng()+"'";
+    		infoContent+='<br><br>'+
+    		'<b>Bearing:</b>&nbsp;'+Math.round(convertHeading(google.maps.geometry.spherical.computeHeading(user_latlng, event.latLng)))+'&deg;<br>'+
+    		'<b>Distance:</b>&nbsp;'+Math.round((google.maps.geometry.spherical.computeDistanceBetween(user_latlng, event.latLng)/1000)*10)/10+'km<br>'+
+    		'<a href="javascript:elevation_profile('+elevation_vars+')"><b>Path Elevation Profile</b></a>';
+    	}
+		infowindow.setContent(infoContent);
+		infowindow.setPosition(event.latLng);
+    	infowindow.open(map);
+	});
 
 	userActiveIcon = new google.maps.MarkerImage("/images/active_user.ico");
 	userAwayIcon = new google.maps.MarkerImage("/images/away_user.ico");
