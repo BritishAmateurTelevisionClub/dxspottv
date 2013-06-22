aaaad04551bebf6cf5f2f39f87c82f8d02ab880d
Phil Crump <phildcrump@gmail.com>
2013-06-23 00:42:14 +0100

    Try loading socketio on DOM load.

--- js/dxspot-websocket.js
+++ js/dxspot-websocket.js
@@ -1,14 +1,16 @@
-var dataSocket = io.connect('http://websocket.dxspot.tv/mapData');
+$(document).ready(function() {
+	var dataSocket = io.connect('http://websocket.dxspot.tv/mapData');
 
-dataSocket.on('mapData', function (data) {
-	dataObject = JSON.parse(data);
-	//console.log(dataObject);
-	loadUsers(dataObject['users']);
-    parseRepeaters(dataObject['repeaters']);
-    parseSpots(dataObject['spots']);
-    createGlobalSpotLog(dataObject['spots']);
-	//socket.emit('my other event', { my: 'data' });
-	checkSpots();
-	checkUsers();
-	checkRepeaters();
-});
+	dataSocket.on('mapData', function (data) {
+		dataObject = JSON.parse(data);
+		//console.log(dataObject);
+		loadUsers(dataObject['users']);
+		parseRepeaters(dataObject['repeaters']);
+		parseSpots(dataObject['spots']);
+		createGlobalSpotLog(dataObject['spots']);
+		//socket.emit('my other event', { my: 'data' });
+		checkSpots();
+		checkUsers();
+		checkRepeaters();
+	});
+}
