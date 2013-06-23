4d1f3b53f2b266b2a219356652385c27dd89116c
Phil Crump <phildcrump@gmail.com>
2013-06-23 03:04:30 +0100

    Remove comments from script.

--- js/dxspot-combine.sh
+++ js/dxspot-combine.sh
@@ -2,12 +2,6 @@
 echo "Removing old files.."
 rm -fv dxspot-combined*
 echo "Combining js.."
-<script src="/js/dxspot-map.js"></script>
-<script src="/js/dxspot-parse.js"></script>
-<script src="/js/dxspot-websocket.js"></script>
-<script src="/js/dxspot-ui.js"></script>
-<script src="/js/atvspot-util.js"></script>
-<script src="/js/locator.js"></script>
 cat dxspot-map.js dxspot-parse.js dxspot-websocket.js dxspot-ui.js atvspot-util.js locator.js > dxspot-combined.js
 echo "gzipping.."
 cp dxspot-combined.js atvspot-combined.js.1
