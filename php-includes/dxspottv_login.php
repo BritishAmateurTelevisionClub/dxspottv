5d05db401e942d6492c934318c3af82da7fdd7e5
Phil Crump <phildcrump@gmail.com>
2013-10-02 13:41:23 +0100

    Add new includes.

---
+++ php-includes/dxspottv_login.php
@@ -0,0 +1,19 @@
+<?php
+$dbc = mysqli_connect('localhost', 'dxspottv', 'RQ5nDCbRHPnQsKaN', 'dxspottv') or die('Error connecting to mysql!:' . mysqli_error($dbc));
+
+function mysql_die()
+{
+	mysqli_close($dbc) or print('Error closing mysql connection!');
+	die();
+}
+
+function mysql_end($dbc)
+{
+	mysqli_close($dbc) or die('Error closing mysql connection!');
+}
+
+function escape($dbc, $str)
+{
+	return addcslashes(mysqli_real_escape_string($dbc,$str), '%_');
+}
+?>
