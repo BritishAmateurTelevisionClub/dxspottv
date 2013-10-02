0eb86ff52e894e386ffd58019366a2805c0f7a42
Phil Crump <phildcrump@gmail.com>
2013-10-02 21:49:15 +0100

    Add php pdo login file.

---
+++ php-includes/dxspottv_pdo.php
@@ -0,0 +1,19 @@
+<?php
+$dbc = new PDO('mysql:host=localhost;dbname=dxspottv', 'dxspottv', 'tCzjqTdLd3PvsyJF', array(
+    PDO::ATTR_PERSISTENT => true
+));
+
+function escape($str) {
+	return addcslashes($str, '%_');
+}
+
+function sha256_salt() {
+	$base64_alphabet='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+	$salt='$5$rounds=1667$';
+ 	for($i=0; $i<13; $i++){
+     	$salt.=$base64_alphabet[mt_rand(0,63)];
+ 	}
+	$salt.='$';
+	return $salt;
+}
+?>
