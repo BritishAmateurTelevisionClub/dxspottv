91785cb128e5278df0dd9ef244034f0fc8269dae
Phil Crump <phildcrump@gmail.com>
2013-10-03 03:58:21 +0100

    Dont automatically set users as radio active when logged in.

--- login.php
+++ login.php
@@ -18,12 +18,6 @@ $crypt = crypt($passwd, $salt);
 
 if($crypt==$target) {
 	$session_key = sha256_salt();
-	
-	$radio_active = 1;
-	$update_statement = $dbc->prepare("UPDATE users set radio_active=? WHERE id=?;");
-	$update_statement->bindValue(1, $radio_active, PDO::PARAM_INT);
-	$update_statement->bindValue(2, $user_id, PDO::PARAM_INT);
-	$update_statement->execute();
 
 	$session_statement = $dbc->prepare("INSERT into sessions (session_id, user_id) VALUES (?,?);");
 	$session_statement->bindValue(1, $session_key, PDO::PARAM_STR);
