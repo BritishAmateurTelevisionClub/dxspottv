ae261e7cf4674ec73d02e02f58a799d33d30801d
Phil Crump <phildcrump@gmail.com>
2013-10-18 23:49:53 +0100

    Remove batc logo.

--- index.php
+++ index.php
@@ -90,7 +90,6 @@ if($logged_in) { ?>
 </head>
 <body>
 <div style="text-align: center; align: top; height: 100%; ">
-<img src="/images/batc-logo2-79px.png" style="height: 79px; width: 98px;" />
 <img src="/images/DXS8.jpg" style="height: 79px; width: 179px; margin-left: 20px;" />
 <table
 style="width: 100%; text-align: left; margin-left: auto; margin-right: auto;"
