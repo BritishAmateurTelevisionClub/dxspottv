c81b294a2b4644e7d19b4f4f0cf4fdccfa4690af
www-data <www-data@dxspot.tv>
2013-10-26 23:19:16 +0000

    Remove data loss notice.

--- index.php
+++ index.php
@@ -90,11 +90,8 @@ if($logged_in) { ?>
 </head>
 <body>
 <div style="text-align: center; align: top; height: 100%; ">
-<div style="margin-left: 200px; margin-right: 200px;">
 <img src="/images/batc-logo2-79px.png" style="height: 79px; width: 98px;" />
 <img src="/images/DXS8.jpg" style="height: 79px; width: 179px; margin-left: 20px;" />
-<font color="red" style="display: block;">Unfortunately due to server management issues, we have experienced a data loss of all new accounts and spots since the 5th October. If you have registered with us since then, please register again! Sorry for the inconvenience, steps are being taken to make sure this doesn't happen again.</font>
-</div>
 <table
 style="width: 100%; text-align: left; margin-left: auto; margin-right: auto;"
 border="0" cellpadding="0" cellspacing="0">
